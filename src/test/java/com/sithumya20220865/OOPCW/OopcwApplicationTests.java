f4550e192ec56baa74262029d6f6af724aa8a6d7
sithumya99 <sithumya20220865@iit.ac.lk>
2024-10-27 14:48:43 +0530

    initialize project

---
+++ src/test/java/com/sithumya20220865/OOPCW/OopcwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sithumya20220865.OOPCW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopcwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
